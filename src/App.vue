20ebf07ee76cd1400ca771dac1da4e91128c3350
Akshay Kumar <akshay.kumar@nbgspl.com>
2022-01-20 19:18:18 +0530

    data other components

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,42 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <home/>
+    <!-- <home/>
+    
 <users/>
-
+<styling/> -->
+<databind/>
+<Props v-bind:user="users"/>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import Home from './components/Home.vue'
-import Users from './components/Users.vue'
+// import Home from './components/Home.vue'
+// import Users from './components/Users.vue'
+// import Styling from './components/Styling.vue'
+import Databind from './components/Databind.vue'
+import Props from './components/Props.vue'
 export default {
   name: 'App',
   components: {
-    Home,
-    Users
+    // Home,
+    // Users,
+    // Styling,
+    Databind,
+    Props
+  },
+  data(){
+     return{
+         users:[
+             {id:1,name:"akshay",email:"akshay@gmai.com"},
+              {id:2,name:"Raj",email:"Raj@gmai.com"},
+               {id:3,name:"Satyam",email:"satyam@gmai.com"},
+                {id:4,name:"mohan",email:"mohan@gmai.com"}
+         ]
+       }
   }
-}
+};
 </script>
 
 <style>

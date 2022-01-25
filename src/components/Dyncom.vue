4c1038928993bda90da0e07a6d3ded9fd68aa99e
Akshay Kumar <akshay.kumar@nbgspl.com>
2022-01-25 19:29:30 +0530

    Add more project

---
+++ src/components/Dyncom.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+ <button @click="tab">Node</button>
+<button @click="tab:'php'">Node</button>
+    <component: is="tab"/>
+</div>
+</template>
+<script>
+import Node from './Node.vue'
+import Php from './Php.vue'
+export default {
+    name:"dyncom",
+    components:{
+        Node,
+        Php
+    },
+    data(){
+        return{
+            tab:"Php"
+        }
+    }
+}
+</script>
